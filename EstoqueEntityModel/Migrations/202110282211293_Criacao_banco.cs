972eb1563d583fe46d7691bab8e04cb00fd9377c
Higor Silvério <higor.silverio@cashme.com.br>
2021-10-28 19:27:33 -0300

    Migrations adicionadas e banco populado

---
+++ EstoqueEntityModel/Migrations/202110282211293_Criacao_banco.cs
@@ -0,0 +1,28 @@
+﻿namespace EstoqueEntityModel.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class Criacao_banco : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProdutoEstoques",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    NumeroProduto = c.String(nullable: false),
+                    NomeProduto = c.String(nullable: false),
+                    DescricaoProduto = c.String(),
+                    EstoqueProduto = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.ProdutoEstoques");
+        }
+    }
+}
--- EstoqueEntityModel/ProvedorEstoque.cs
+++ EstoqueEntityModel/ProvedorEstoque.cs
@@ -1,31 +1,35 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
-using System.Linq;
 
 namespace EstoqueEntityModel
 {
     public class ProvedorEstoque : DbContext
     {
-        // Your context has been configured to use a 'ProvedorEstoque' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'EstoqueEntityModel.ProvedorEstoque' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'ProvedorEstoque' 
-        // connection string in the application configuration file.
+
         public ProvedorEstoque()
             : base("name=ProvedorEstoque")
         {
         }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+        public virtual DbSet<ProdutoEstoque> ProdutosEstoque { get; set; }
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
+    public class ProdutoEstoque
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O número do produto é mandatório")]
+        public string NumeroProduto { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é mandatório")]
+        public string NomeProduto { get; set; }
+
+        public string DescricaoProduto { get; set; }
+
+        [Required(ErrorMessage = "O número do produtos no estoque é mandatório")]
+        public int EstoqueProduto { get; set; }
+    }
+
 }
